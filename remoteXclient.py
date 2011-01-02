b177ac1c34b02a1ad98ba12d8ca5969efd275fcf
Bryan Burke <bryan.t.burke@gmail.com>
2011-01-02 19:23:46 +0000

    Send bulk commands as one message to server

--- remoteXclient.py
+++ remoteXclient.py
@@ -6,21 +6,29 @@ import pickle
 class remoteXclient:
 
     def __init__(self):
-        self.socketup()
-        if not self.serverup:
-            print "RemoteX Client: server not found. Please start the server on your local machine."
-        self.socketdown()
+        self.cmdstring_list = []
+        #self.socketup()
+        #if not self.serverup:
+        #    print "RemoteX Client: server not found. Please start the server on your local machine."
+        #self.socketdown()
 
     def __getattr__(self, func):
         def interceptmethod(*args, **kwargs):
-            self.socketup()
+            #self.socketup()
             cmdstring = {'func': func, 'args': args, 'kwargs': kwargs}
-            if self.serverup:
-                self.socketobj.send(pickle.dumps(cmdstring,2))
-                self.socketdown()
-            else:
-                print "RemoteX Client: server is down, cannot forward."
-                print cmdstring
+            self.cmdstring_list.append(cmdstring)
+            if func == 'show':
+                self.socketup()
+                if self.serverup:
+                    self.socketobj.send(pickle.dumps(self.cmdstring_list,2))
+                    self.socketdown()
+                else:
+                    print "RemoteX Client: server is down, cannot forward."
+                    print cmdstring
+                self.cmdstring_list = []
+            #else:
+             #   self.cmdstring_list.append(cmdstring)
+
         return interceptmethod
 
     def socketup(self, ip='', port=9898):
@@ -36,5 +44,10 @@ class remoteXclient:
 
 if __name__ == '__main__':
     remX = remoteXclient()
+    remX.figure()
     remX.plot(range(20),range(20),linewidth=0.5)
-
+    remX.show()
+    remX.figure()
+    remX.plot(range(10), range(10))
+    remX.title('Test Title, 2nd Figure')
+    remX.show()
